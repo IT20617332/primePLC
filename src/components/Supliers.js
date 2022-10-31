562c15e9f292bade8f6ed3c8cfcc7d258393846c
IT20617332 <siridana@gmail.com>
2022-10-31 11:18:06 +0530

    Suppliers table modified

--- src/components/Supliers.js
+++ src/components/Supliers.js
@@ -6,117 +6,108 @@ import { Table } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
 const Supliers = () => {
-  var [suplierObjects, setsuplierObjects] = useState({})
-  var [currentId, setCurrentId] = useState('')
+  var [suplierObjects, setsuplierObjects] = useState({});
+  var [currentId, setCurrentId] = useState("");
 
-  const addOrEdit = obj => {
-    if (currentId == '')
-      firebaceDb.child('suppliers').push(
-        obj,
-        err => {
-          if (err)
-            console.log(err)
-          else
-            setCurrentId('')
-        }
-      )
+  const addOrEdit = (obj) => {
+    if (currentId == "")
+      firebaceDb.child("suppliers").push(obj, (err) => {
+        if (err) console.log(err);
+        else setCurrentId("");
+      });
     else
-      firebaceDb.child(`suppliers/${currentId}`).set(
-        obj,
-        err => {
-          if (err)
-            console.log(err)
-
-          else
-            setCurrentId('')
-        }
-      )
-  }
+      firebaceDb.child(`suppliers/${currentId}`).set(obj, (err) => {
+        if (err) console.log(err);
+        else setCurrentId("");
+      });
+  };
   useEffect(() => {
-    firebaceDb.child('suppliers').on('value', snapshot => {
+    firebaceDb.child("suppliers").on("value", (snapshot) => {
       if (snapshot.val() != null)
         setsuplierObjects({
-          ...snapshot.val()
-
-        })
-    })
-
-  }, [])
-  const onDelete = key => {
-    if (window.confirm('Are You Shure To Delete this Supplier ? ')) {
-      firebaceDb.child(`suppliers/${key}`).remove(
-        err => {
-          if (err)
-            console.log(err)
-          else
-            setCurrentId('')
-        }
-      )
-
-
+          ...snapshot.val(),
+        });
+    });
+  }, []);
+  const onDelete = (key) => {
+    if (window.confirm("Are You Shure To Delete this Supplier ? ")) {
+      firebaceDb.child(`suppliers/${key}`).remove((err) => {
+        if (err) console.log(err);
+        else setCurrentId("");
+      });
     }
-
-
-  }
+  };
 
   return (
     <div>
-      <div style={{ backgroundColor: "#AEAEAE", color: "black", paddingTop: "8vh", paddingBottom: "10vh", paddingLeft: "4vh" }} >
-      <a href="/"><Button variant="outline-dark" >Back</Button></a>
+      <div
+        style={{
+          backgroundColor: "#AEAEAE",
+          color: "black",
+          paddingTop: "8vh",
+          paddingBottom: "10vh",
+          paddingLeft: "4vh",
+        }}
+      >
+        <a href="/">
+          <Button variant="outline-dark">Back</Button>
+        </a>
 
         <Container>
           <Row>
-            <Col><Suplierform {...({ addOrEdit, currentId, suplierObjects })} /></Col>
+            <Col>
+              <Suplierform {...{ addOrEdit, currentId, suplierObjects }} />
+            </Col>
             <Col>
               <div style={{ paddingLeft: "8vh" }}>
                 <h3 data-testid="supheading">Suppliers:</h3>
               </div>
               <div style={{ paddingLeft: "7vh", paddingTop: "5vh" }}>
-                <Table striped bordered hover style={{ border: "black" }} >
-                  <thead  >
+                <Table striped bordered hover style={{ border: "black" }}>
+                  <thead>
                     <tr>
-                      <th>Contact Number</th>
-                      <th>Supplier Address</th>
                       <th>Supplier Name</th>
-                      <th>Acctions</th>
-
+                      <th>Supplier Address</th>
+                      <th>Contact Number</th>
+                      <th>Actions</th>
                     </tr>
                   </thead>
                   <tbody>
-
-                    {Object.keys(suplierObjects).map(id => {
-                      return <tr key={id}>
-                        <td>{suplierObjects[id].suppliername}</td>
-                        <td>{suplierObjects[id].supplieraddress}</td>
-                        <td>{suplierObjects[id].contactno}</td>
-                        <td>
-                          <a className="btn text-primary" onClick={() => { setCurrentId(id) }} >
-
-                            <i className="fas fa-pencil-alt"></i>
-                          </a>
-                          <a className="btn text-danger" onClick={() => { onDelete(id) }} >
-
-                            <i className="fas fa-trash-alt"></i>
-                          </a>
-                        </td>
-                      </tr>
+                    {Object.keys(suplierObjects).map((id) => {
+                      return (
+                        <tr key={id}>
+                          <td>{suplierObjects[id].suppliername}</td>
+                          <td>{suplierObjects[id].supplieraddress}</td>
+                          <td>{suplierObjects[id].contactno}</td>
+                          <td>
+                            <a
+                              className="btn text-primary"
+                              onClick={() => {
+                                setCurrentId(id);
+                              }}
+                            >
+                              <i className="fas fa-pencil-alt"></i>
+                            </a>
+                            <a
+                              className="btn text-danger"
+                              onClick={() => {
+                                onDelete(id);
+                              }}
+                            >
+                              <i className="fas fa-trash-alt"></i>
+                            </a>
+                          </td>
+                        </tr>
+                      );
                     })}
-
                   </tbody>
-
                 </Table>
               </div>
             </Col>
           </Row>
         </Container>
-
-
       </div>
-
     </div>
-
-
   );
-
-}
+};
 export default Supliers;
